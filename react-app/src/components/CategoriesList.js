eb990ea19bcb4fa8b11426bcc8bc2ce60f6f1822
KumudRajGhimire <kumudraj.cs23@bmsce.ac.in>
2025-01-26 10:27:41 +0530

    Delete react-app directory

--- react-app/src/components/CategoriesList.js
+++
@@ -1,4 +0,0 @@
-
-let Categories = ['Stationaries', 'Entertainment', 'Fashion','Furniture', 'Cutleries', 'Vehicles', 'Hobbies', 'Sports', 'Daily', 'Electronics', 'Notes', 'Miscellaneous']
-
-export default Categories;
